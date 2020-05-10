6623dab8e168220a39bddb3c18dd25d47dc1b2ec
De'on Summers <deonsummers91@gmail.com>
2020-05-10 13:13:46 -0500

    initial web3

--- client/flash4all/src/App.js
+++ client/flash4all/src/App.js
@@ -3,10 +3,12 @@ import logo from './logo.svg';
 import TokenList from './components/TokenList';
 import { Home } from './pages';
 import './App.css';
+import Web3 from 'web3';
 
 import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 
 function App() {
+
   return (
     <div className="App">
       <Router>
--- client/flash4all/src/pages/Home.js
+++ client/flash4all/src/pages/Home.js
@@ -1,13 +1,17 @@
 import React, {Component}  from 'react';
 import { connect } from 'react-redux';
 import tokenActions from '../redux/actions/tokenActions';
+import Web3 from 'web3';
 
 let { fetchTokens, addToken, arbitrage }  = tokenActions;
+
+let abi = {};
 
 class Home extends Component {
   constructor(props) {
     super(props);
     this.state = {
+      flashEthAmount: 0,
     }
   }
 
@@ -19,7 +23,21 @@ class Home extends Component {
     this.props.arbitrage(token);
   }
 
+  flashEth = () => {
+    console.log(this.state.flashEthAmount + 'flash eth clicked');
+    window.ethereum.enable().then((accounts) => {
+      console.log(accounts[0])
+    });
+  }
+
   componentDidMount() {
+    if (window.web3 == null) {
+      var web3 = new Web3(Web3.givenProvider);
+      window.web3 = web3;
+    }
+
+    console.log(window.web3);
+
     this.props.fetchTokens();
     this.props.arbitrage('ETH');
   }
@@ -35,13 +53,41 @@ class Home extends Component {
        <br/>
 
        {trades.map((trade) => (<div> {JSON.stringify(trade)}</div>))}
+       <hr />
+       <hr />
+       <div class="input-group i-1">
+         <input type="text" onChange={(e) => this.setState({flashEthAmount: e.target.value})} class="form-control" id="input1" placeholder="Amount of ETH" />
+         <button onClick={this.flashEth} type="button" id="data_button1" class="btn btn-primary">Flash ETH</button>
+       </div>
+       <div class="input-group i-2">
+         <input type="text" class="form-control" id="input2" placeholder="Amount of DAI" />
+         <button type="button" id="data_button2" class="btn btn-primary">Flash DAI</button>
+       </div>
+       <div class="input-group i-3">
+         <input type="text" class="form-control" id="input3" placeholder="Amount of DAI" />
+         <button type="button" id="data_button3" class="btn btn-primary">Flash DAI > KNC > ETH (via Kyber)</button>
+       </div>
+       <div class="input-group i-3">
+         <input type="text" class="form-control" id="input4" placeholder="Amount of DAI" />
+         <button type="button" id="data_button4" class="btn btn-primary">Flash DAI > KNC > ETH (via Uniswap v2 flash?)</button>
+       </div>
+       <div class="input-group i-4">
+         <input type="text" class="form-control" id="input5_1" placeholder="Amount of DAI" />
+         <input type="text" class="form-control" id="input5_2" placeholder="Flashloan amount in DAI" />
+         <button type="button" id="data_button5" class="btn btn-primary">Flash DAI > KNC > ETH (via Kyber)</button>
+       </div>
+
+       <div class="input-group i-5">
+         <input type="text" class="form-control" id="input6" placeholder="Amount of DAI" />
+         <button type="button" id="data_button5" class="btn btn-primary">Flash DAI > EOS > DAI (via predefined exchanges x,y)</button>
+       </div>
      </section>
      )
    }
 }
 
 const mapStateToProps = state => ({
- ...state
+  ...state
 })
 
 const mapDispatchToProps = dispatch => ({
