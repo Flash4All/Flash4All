3f75dd9795be94c6b4269420ab0007d1185d108d
De'on Summers <deonsummers91@gmail.com>
2020-05-15 12:59:44 -0500

    add kyber

--- trade_forex/dex_data.py
+++ trade_forex/dex_data.py
@@ -1,8 +1,8 @@
 import requests
 from decimal import Decimal
-import { ... } from '@uniswap/sdk'
 import json
 from redis_client import redis_client
+
 #market_prices = requests.get(f'https://api.kyber.network/market')
 addresses = {
             'MKR': '0x9f8f72aa9304c8b593d555f12ef6589cc3a579a2',
@@ -11,10 +11,10 @@ addresses = {
             'TUSD': '0x8dd5fbce2f6a956c3022ba3663759011dd51e73e',
             'USDT': '0xdac17f958d2ee523a2206206994597c13d831ec7',
             'SAI': '0x89d24a6b4ccb1b6faa2625fe562bdd9a23260359',
-            'ETH': '0xc02aaa39b223fe8d0a0e5c4f27ead9083c756cc2',
-            'REP': '0xe94327d07fc17907b4db788e5adf2ed424addff6'
+            'ETH': '0xc02aaa39b223fe8d0a0e5c4f27ead9083c756cc2'
         }
 
+token_list = [t for t in requests.get('https://flash4all.net/api/tokens').json() if t != "REP"]
 
 input_flash_collateral = 'MKR'
 #input_flash_collateral = input('Enter Flash Collateral').upper()
@@ -36,48 +36,40 @@ sell_or_buy_1 = 'sell'
 sell_or_buy_2 = 'sell'
 sell_or_buy_3 = 'sell'
 print()
-forward_trade_1 = requests.get(f'https://api.kyber.network/quote_amount?base={flash_collateral_address}&quote={token_2}&base_amount={collateral_total}&type={sell_or_buy_1}').json()
-trade_2_quantity = forward_trade_1['data']
-print(f'I now have {trade_2_quantity} {trade_path}')
-
-forward_trade_2 =  requests.get(f'https://api.kyber.network/quote_amount?base={token_2}&quote={token_3}&base_amount={trade_2_quantity}&type={sell_or_buy_2}').json()
-trade_3_quantity = forward_trade_2['data']
-print(f'I now have {trade_3_quantity} {trade_path_2}')
-forward_trade_3 = requests.get(f'https://api.kyber.network/quote_amount?base={token_3}&quote={flash_collateral_address}&base_amount={trade_3_quantity}&type={sell_or_buy_3}').json()
-end_result = forward_trade_3['data']
-print(f'I now have {end_result} of {input_flash_collateral}')
-
-before = (Decimal(forward_trade_1['data']))
-middle = (Decimal(forward_trade_2['data']))
-after = (Decimal(forward_trade_3['data']))
-
-quick_calc = after - 1
-ratio_calculation = ((after/1)-1 )*100
-print(ratio_calculation)
-print(quick_calc)
 
 
-def find_reversal()
+def find_reversal():
+    pass
 """
 #1inch_token_list = no token list call
 #([x['symbol'] for x in token_list])
+"""
 
+def find_uniswap_bid_ask(token, token2):
+    pass
 
-def find_bid_ask(token, token2):
+def find_kyber_bid_ask(token, token2):
+    token_1_address = addresses[token]
+    token_2_address = addresses[token2]
     try:
-        bid_price1 = requests.get(f'https://api-v2.dex.ag/price?from={token2}&to={token}&fromAmount=1&dex=ag').json()
-        ask_price1 = requests.get(f'https://api-v2.dex.ag/price?from={token}&to={token2}&fromAmount=1&dex=ag').json()
-        ask_float = Decimal(ask_price1['price'])
-        bid_float = 1/Decimal(bid_price1['price'])
-        redis_client.set(f'{token},{token2}',f'{bid_float},{ask_float}')
+        buy_side = requests.get(f'https://api.kyber.network/quote_amount?base={token_1_address}&quote={token_2_address}&base_amount={1}&type=buy').json()
+        sell_side = requests.get(f'https://api.kyber.network/quote_amount?base={token_1_address}&quote={token_2_address}&base_amount={1}&type=sell').json()
+        if buy_side['error'] == True or sell_side['error'] == True:
+            raise
+        print(f'{token}->{token2}: Buy {buy_side}, Sell {sell_side}')
+        bid_float = sell_side['data']
+        ask_float = buy_side['data']
+        redis_client.set(f'kyber,{token},{token2}',f'{bid_float},{ask_float}')
     except Exception as e:
         print(e)
+
 for token in token_list:
-    [find_bid_ask(token, token2) for token2 in token_list if token != token2]
+    [find_kyber_bid_ask(token, token2) for token2 in token_list if token != token2]
+    [find_uniswap_bid_ask(token, token2) for token2 in token_list if token != token2]
 
 
-print(redis_client.get('DAI,ETH'))
-#"""
+print(redis_client.get('DAI,ETH').decode())
+print(redis_client.get('ETH,DAI').decode())
 
 
 
