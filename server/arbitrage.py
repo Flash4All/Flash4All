9969a639baa3c2427ce065c2477cecf8e57e7c6c
De'on Summers <deonsummers91@gmail.com>
2020-05-23 11:58:01 -0500

    update server

--- server/arbitrage.py
+++ server/arbitrage.py
@@ -2,10 +2,12 @@ import requests
 from decimal import Decimal
 from flask import Blueprint, render_template, abort, jsonify, request
 from redis_client import redis_client
+import sys, os
 
 arbitrage_routes = Blueprint('arbitrage', __name__)
 
 token_list = requests.get('https://flash4all.net/api/tokens').json()
+MARKETS=["uniswap", "kyber"]
 
 @arbitrage_routes.route('', methods=["POST"])
 def arbitrage_index():
@@ -17,20 +19,52 @@ def arbitrage_index():
         if token2 != token1:
             for token3 in token_list:
                 if token3 != token1 and token3 != token2:
-                    print(token3, token2)
                     arbitriage_opps.append(calculate_forward(token1, token2, token3))
+                    # arbitriage_opps.append(calculate_reversal(token1, token2, token3))
     arbitriage_opps.sort(reverse=True, key = get_tuple_calcutions)
-    return jsonify(result=arbitriage_opps[:3])
+    return jsonify(result=arbitriage_opps[:5])
 
 
 def calculate_forward(token1, token2, token3):
-    trade_1 = redis_client.get(f'{token1},{token2}').decode().split(',')
-    trade_2 = redis_client.get(f'{token3},{token2}').decode().split(',')
-    trade_3 = redis_client.get(f'{token1},{token3}').decode().split(',')
-    forward_calculation = ((Decimal(trade_1[0])) * (1/Decimal(trade_2[1])) * (1/Decimal(trade_3[1])))-1
-    forward_calculation_percent = forward_calculation * 100
-    return (f'{token1},{token2},{token3}', forward_calculation_percent)
+    trade_1 = None
+    trade_2 = None
+    trade_3 = None
+    for first_trade_market in MARKETS:
+        for second_trade_market in MARKETS:
+            for third_trade_market in MARKETS:
+                try:
+                    trade_1 = redis_client.get(f'{first_trade_market},{token1},{token2}').decode().split(',')
+                    trade_2 = redis_client.get(f'{second_trade_market},{token3},{token2}').decode().split(',')
+                    trade_3 = redis_client.get(f'{third_trade_market},{token1},{token3}').decode().split(',')
+                    forward_calculation = ((Decimal(trade_1[0])) * (1 / Decimal(trade_2[1])) * (1 / Decimal(trade_3[1]))) - 1
+                    forward_calculation_percent = forward_calculation * 100
+                    return (f'{first_trade_market},{second_trade_market},{third_trade_market},{token1},{token2},{token3}', forward_calculation_percent)
+                except Exception as e:
+                    if "ZRX" in [token1, token2, token3]:
+                        return (f'{token1},{token2},{token3}', -1000000)
+                    if "OMG" in [token1, token2, token3]:
+                        return (f'{token1},{token2},{token3}', -1000000)
+                    exc_type, exc_obj, exc_tb = sys.exc_info()
+                    fname = os.path.split(exc_tb.tb_frame.f_code.co_filename)[1]
+                    print(exc_type, fname, exc_tb.tb_lineno)
+                    return (f'{token1},{token2},{token3}', -1000000)
+
+def calculate_reversal(token1, token2, token3):
+    pass
+    # try:
+    #     trade_1 = redis_client.get(f'{market},{token1},{token2}').decode().split(',')
+    #     trade_2 = redis_client.get(f'{market},{token3},{token2}').decode().split(',')
+    #     trade_3 = redis_client.get(f'{market},{token1},{token3}').decode().split(',')
+    #     reversal_calculation = (1 / (Decimal(trade_1[1])) * (Decimal(trade_2[0])) * (Decimal(trade_3[0]))) - 1
+    #     reversal_calculation_percent = reversal_calculation * 100
+    #     return f'{token1},{token2},{token3}', reversal_calculation_percent
+    # except Exception as e:
+    #     exc_type, exc_obj, exc_tb = sys.exc_info()
+    #     fname = os.path.split(exc_tb.tb_frame.f_code.co_filename)[1]
+    #     print(exc_type, fname, exc_tb.tb_lineno)
+    #     return {'error': str(e)}
+    #
 
 def get_tuple_calcutions(item):
+    print(item)
     return item[1]
-
