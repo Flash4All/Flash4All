a9a4ee576bbba0fb97bb6709f0bdfe3f31619ada
user <user@users-MacBook-Air.local>
2020-05-24 10:42:46 -0400

    they said this be easy

--- trade_forex/dex_data.py
+++ trade_forex/dex_data.py
@@ -22,19 +22,6 @@ addresses = {
             'KNC': '0xdd974d5c2e2928dea5f71b9825b8b646686bd200'
 }
 
-uniswap_addresses = {
-            'DAI': 'ETH_0x97deC872013f6B5fB443861090ad931542878126',
-            'USDC': 'ETH_0xe9Cf7887b93150D4F2Da7dFc6D502B216438F244',
-            'MKR': 'ETH_0x4d2f5cFbA55AE412221182D8475bC85799A5644b',
-            'WBTC': 'ETH_0xA2881A90Bf33F03E7a3f803765Cd2ED5c8928dFb',
-            'Link': 'ETH_0x2E642b8D59B45a1D8c5aEf716A84FF44ea665914',
-            'BAT' : 'ETH_0x3958B4eC427F8fa24eB60F42821760e88d485f7F',
-            'ETH' : 'ETH_0x05cDe89cCfa0adA8C88D5A23caaa79Ef129E7883',
-            'TUSD': 'ETH_0x1aEC8F11A7E78dC22477e91Ed924Fab46e3A88Fd',
-            'USDT' : 'Eth_0x000000x000x0x0x00x0x0x0x0x0x0x0x00x0xx0',
-            'SAI' : 'Eth_0x000000x000x0x0x00x0x0x0x0x0x0x0x00x0xx0'
-}
-
 token_list = [t for t in requests.get('https://flash4all.net/api/tokens').json() if t != "REP"]
 
 input_flash_collateral = 'MKR'
@@ -51,14 +38,17 @@ def find_uniswap_bid_ask(token, token2):
         return
 
     dex_ = 'uniswap'
-    initial_amount = 10
+    initial_amount = Decimal(100)
+
     try:
     #bid price = sell price
     # buy side = ask price should be lower
         uniswap_sell_side = requests.get(
-        f'https://api-v2.dex.ag/price?from={token}&to={token2}&fromAmount={initial_amount}&dex={dex_}&limitAmount=').json()
+        f'https://api-v2.dex.ag/price?from={token}&to={token2}&fromAmount={initial_amount}&dex={dex_}').json()
+        uniswap_token2_amount = Decimal(uniswap_sell_side.get('price')) * initial_amount
+
         uniswap_buy_side = requests.get(
-        f'https://api-v2.dex.ag/price?from={token2}&to={token}&fromAmount={initial_amount}&dex={dex_}&limitAmount=').json()
+        f'https://api-v2.dex.ag/price?from={token2}&to={token}&fromAmount={uniswap_token2_amount}&dex={dex_}').json()
 
         uniswap_bid_float = uniswap_sell_side.get('price')
         uniswap_ask_float = (1 / Decimal(uniswap_buy_side.get('price')))
@@ -73,19 +63,30 @@ def find_uniswap_bid_ask(token, token2):
 
 
 def find_kyber_bid_ask(token, token2):
-    token_1_address = addresses[token]
-    token_2_address = addresses[token2]
+    if token == 'SAI' or token2 == 'SAI' or token == 'USDT' or token2 == 'USDT' or token == 'OMG' or token2 == 'OMG':
+        return
+
+    dex_2 = "kyber"
+    initial_amount: int = 100
     try:
-        buy_side = requests.get(f'https://api.kyber.network/quote_amount?base={token_1_address}&quote={token_2_address}&base_amount={10}&type=buy').json()
-        sell_side = requests.get(f'https://api.kyber.network/quote_amount?base={token_1_address}&quote={token_2_address}&base_amount={10}&type=sell').json()
-        #if buy_side['error'] == True or sell_side['error'] == True:
-         #   raise
-        print(f'{token}->{token2}: Buy {buy_side}, Sell {sell_side}')
-        bid_float = sell_side['data']
-        ask_float = buy_side['data']
-        redis_client.set(f'kyber,{token},{token2}',f'{bid_float},{ask_float}')
+        kyber_sell_side = requests.get(
+            f'https://api-v2.dex.ag/price?from={token}&to={token2}&fromAmount={initial_amount}&dex={dex_2}&limitAmount=').json()
+        kyber_token2_amount = Decimal(kyber_sell_side.get('price')) * initial_amount
+        kyber_buy_side = requests.get(
+            f'https://api-v2.dex.ag/price?from={token2}&to={token}&fromAmount={kyber_token2_amount}&dex={dex_2}&limitAmount=').json()
+
+
+
+        kyber_bid_float = kyber_sell_side.get('price')
+        kyber_ask_float = (1 / Decimal(kyber_buy_side.get('price')))
+        print(f'{token}->{token2}: Buy {kyber_ask_float}, Sell {kyber_bid_float}')
+        redis_client.set(f'kyber,{token},{token2}',f'{kyber_bid_float},{kyber_ask_float}')
     except Exception as e:
-        print(e)
+        print(token, token2)
+        exc_type, exc_obj, exc_tb = sys.exc_info()
+        fname = os.path.split(exc_tb.tb_frame.f_code.co_filename)[1]
+        print(exc_type, fname, exc_tb.tb_lineno)
+        return {'error': str(e)}
 
 for token in token_list:
     [find_kyber_bid_ask(token, token2) for token2 in token_list if token != token2]
--- trade_forex/triangle_forward_and_reversal.py
+++ trade_forex/triangle_forward_and_reversal.py
@@ -17,6 +17,7 @@ MARKETS=["uniswap", "kyber"]
 # forward Triangle = Bid_Pair 1 * (1/ASK_Pair2) * (1/ASK_Pair3)
 
 def calculate_forward(token1, token2, token3):
+    results = []
     trade_1 = None
     trade_2 = None
     trade_3 = None
@@ -25,11 +26,14 @@ def calculate_forward(token1, token2, token3):
             for third_trade_market in MARKETS:
                 try:
                     trade_1 = redis_client.get(f'{first_trade_market},{token1},{token2}').decode().split(',')
+                    print('_____')
+                    print(first_trade_market, second_trade_market, third_trade_market)
+
                     trade_2 = redis_client.get(f'{second_trade_market},{token3},{token2}').decode().split(',')
                     trade_3 = redis_client.get(f'{third_trade_market},{token1},{token3}').decode().split(',')
                     forward_calculation = ((Decimal(trade_1[0])) * (1 / Decimal(trade_2[1])) * (1 / Decimal(trade_3[1]))) - 1
                     forward_calculation_percent = forward_calculation * 100
-                    return (f'{first_trade_market},{second_trade_market},{third_trade_market},{token1},{token2},{token3}', forward_calculation_percent)
+                    results.append(f'{first_trade_market},{second_trade_market},{third_trade_market},{token1},{token2},{token3}', forward_calculation_percent)
                 except Exception as e:
                     if "ZRX" in [token1, token2, token3]:
                         return (f'{token1},{token2},{token3}', -1000000)
@@ -43,7 +47,7 @@ def calculate_forward(token1, token2, token3):
                     print(exc_type, fname, exc_tb.tb_lineno)
                     print('__________________________')
                     return (f'{token1},{token2},{token3}', -1000000)
-
+    return results
 def calculate_reversal(token1, token2, token3):
     try:
         trade_1 = redis_client.get(f'{market},{token1},{token2}').decode().split(',')
@@ -73,9 +77,9 @@ for token2 in token_list:
                 arbitriage_opps.append(calculate_forward(token1, token2, token3))
                 #arbitriage_opps.append(calculate_reversal(token1, token2, token3))
 
-print(arbitriage_opps)
+
 arbitriage_opps.sort(reverse=True, key=get_tuple_calcutions)
-print(arbitriage_opps[:5])
+print(arbitriage_opps)
 
 """
 #reversal calculation = Bid3* BID2 *(1/ASK1)
