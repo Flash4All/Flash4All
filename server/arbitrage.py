0bc8bd3075cd3358d59607c2f8860b883c5594b1
De'on Summers <deonsummers91@gmail.com>
2020-05-29 08:47:52 -0500

    add trade to server

--- server/arbitrage.py
+++ server/arbitrage.py
@@ -25,29 +25,36 @@ def arbitrage_index():
     return jsonify(result=arbitriage_opps[:5])
 
 
-def calculate_forward(token1, token2, token3):
-    trade_1 = None
-    trade_2 = None
-    trade_3 = None
-    for first_trade_market in MARKETS:
-        for second_trade_market in MARKETS:
-            for third_trade_market in MARKETS:
-                try:
-                    trade_1 = redis_client.get(f'{first_trade_market},{token1},{token2}').decode().split(',')
-                    trade_2 = redis_client.get(f'{second_trade_market},{token3},{token2}').decode().split(',')
-                    trade_3 = redis_client.get(f'{third_trade_market},{token1},{token3}').decode().split(',')
-                    forward_calculation = ((Decimal(trade_1[0])) * (1 / Decimal(trade_2[1])) * (1 / Decimal(trade_3[1]))) - 1
-                    forward_calculation_percent = forward_calculation * 100
-                    return (f'{first_trade_market},{second_trade_market},{third_trade_market},{token1},{token2},{token3}', forward_calculation_percent)
-                except Exception as e:
-                    if "ZRX" in [token1, token2, token3]:
-                        return (f'{token1},{token2},{token3}', -1000000)
-                    if "OMG" in [token1, token2, token3]:
-                        return (f'{token1},{token2},{token3}', -1000000)
-                    exc_type, exc_obj, exc_tb = sys.exc_info()
-                    fname = os.path.split(exc_tb.tb_frame.f_code.co_filename)[1]
-                    print(exc_type, fname, exc_tb.tb_lineno)
-                    return (f'{token1},{token2},{token3}', -1000000)
+def calculate_forward(token1, token2, token3=None):
+   results = []
+   trade_1 = None
+   trade_2 = None
+   trade_3 = None
+   for first_trade_market in MARKETS:
+       trade_1 = redis_client.get(f'{first_trade_market},{token1},{token2}').decode().split(',')
+       for second_trade_market in MARKETS:
+           if token3 is None: 
+               trade_2 = redis_client.get(f'{second_trade_market},{token2},{token1}').decode().split(',')
+               forward_calculation = ((Decimal(trade_1[0])) * (1 / Decimal(trade_2[1])) ) - 1
+               forward_calculation_percent = forward_calculation * 100
+               results.append((f'{first_trade_market},{second_trade_market},{token1},{token2}', forward_calculation_percent))
+           for third_trade_market in MARKETS:
+               try:
+                   if token3 is not None:
+                       trade_2 = redis_client.get(f'{second_trade_market},{token3},{token2}').decode().split(',')
+                       trade_3 = redis_client.get(f'{third_trade_market},{token1},{token3}').decode().split(',')
+                       forward_calculation = ((Decimal(trade_1[0])) * (1 / Decimal(trade_2[1])) * (1 / Decimal(trade_3[1]))) - 1
+                       forward_calculation_percent = forward_calculation * 100
+                       results.append((f'{first_trade_market},{second_trade_market},{third_trade_market},{token1},{token2},{token3}', forward_calculation_percent))
+               except Exception as e:
+                   print(first_trade_market, second_trade_market, third_trade_market)
+                   print(f'TOKENS: {token1}, {token2}, {token3}')
+                   print(f'TRADES: {trade_1}, {trade_2}, {trade_3}')
+                   exc_type, exc_obj, exc_tb = sys.exc_info()
+                   fname = os.path.split(exc_tb.tb_frame.f_code.co_filename)[1]
+                   print(exc_type, fname, exc_tb.tb_lineno)
+                   print('__________________________')
+   return results
 
 def calculate_reversal(token1, token2, token3):
     pass
