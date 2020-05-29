3f61af09266a340125a34946d7d3480aed44b2f2
Xanderall <64374557+Xanderall@users.noreply.github.com>
2020-05-29 22:59:02 +0200

    code optimizations

--- contracts/flashturnaround.sol
+++ contracts/flashturnaround.sol
@@ -20,51 +20,11 @@ interface SimpleNetworkInterface { //Kyber on Ropsten 0x818E6FECD516Ecc3849DAf68
 
 interface UniswapFactoryInterface {
     function getExchange(address token) external view returns (address exchange);
-    function getToken(address exchange) external view returns (address token);
-    function getTokenWithId(uint256 tokenId) external view returns (address token);
 }
 interface UniswapExchangeInterface {
-    // Address of ERC20 token sold on this exchange
-    function tokenAddress() external view returns (address token);
-    // Address of Uniswap Factory
-    function factoryAddress() external view returns (address factory);
-    // Get Prices
-    function getEthToTokenInputPrice(uint256 eth_sold) external view returns (uint256 tokens_bought);
-    function getEthToTokenOutputPrice(uint256 tokens_bought) external view returns (uint256 eth_sold);
-    function getTokenToEthInputPrice(uint256 tokens_sold) external view returns (uint256 eth_bought);
-    function getTokenToEthOutputPrice(uint256 eth_bought) external view returns (uint256 tokens_sold);
-    // Trade ETH to ERC20
     function ethToTokenSwapInput(uint256 min_tokens, uint256 deadline) external payable returns (uint256  tokens_bought);
-    function ethToTokenTransferInput(uint256 min_tokens, uint256 deadline, address recipient) external payable returns (uint256  tokens_bought);
-    function ethToTokenSwapOutput(uint256 tokens_bought, uint256 deadline) external payable returns (uint256  eth_sold);
-    function ethToTokenTransferOutput(uint256 tokens_bought, uint256 deadline, address recipient) external payable returns (uint256  eth_sold);
-    // Trade ERC20 to ETH
     function tokenToEthSwapInput(uint256 tokens_sold, uint256 min_eth, uint256 deadline) external returns (uint256  eth_bought);
-    function tokenToEthTransferInput(uint256 tokens_sold, uint256 min_eth, uint256 deadline, address recipient) external returns (uint256  eth_bought);
-    function tokenToEthSwapOutput(uint256 eth_bought, uint256 max_tokens, uint256 deadline) external returns (uint256  tokens_sold);
-    function tokenToEthTransferOutput(uint256 eth_bought, uint256 max_tokens, uint256 deadline, address recipient) external returns (uint256  tokens_sold);
-    // Trade ERC20 to ERC20
     function tokenToTokenSwapInput(uint256 tokens_sold, uint256 min_tokens_bought, uint256 min_eth_bought, uint256 deadline, address token_addr) external returns (uint256  tokens_bought);
-    function tokenToTokenTransferInput(uint256 tokens_sold, uint256 min_tokens_bought, uint256 min_eth_bought, uint256 deadline, address recipient, address token_addr) external returns (uint256  tokens_bought);
-    function tokenToTokenSwapOutput(uint256 tokens_bought, uint256 max_tokens_sold, uint256 max_eth_sold, uint256 deadline, address token_addr) external returns (uint256  tokens_sold);
-    function tokenToTokenTransferOutput(uint256 tokens_bought, uint256 max_tokens_sold, uint256 max_eth_sold, uint256 deadline, address recipient, address token_addr) external returns (uint256  tokens_sold);
-    // Trade ERC20 to Custom Pool
-    function tokenToExchangeSwapInput(uint256 tokens_sold, uint256 min_tokens_bought, uint256 min_eth_bought, uint256 deadline, address exchange_addr) external returns (uint256  tokens_bought);
-    function tokenToExchangeTransferInput(uint256 tokens_sold, uint256 min_tokens_bought, uint256 min_eth_bought, uint256 deadline, address recipient, address exchange_addr) external returns (uint256  tokens_bought);
-    function tokenToExchangeSwapOutput(uint256 tokens_bought, uint256 max_tokens_sold, uint256 max_eth_sold, uint256 deadline, address exchange_addr) external returns (uint256  tokens_sold);
-    function tokenToExchangeTransferOutput(uint256 tokens_bought, uint256 max_tokens_sold, uint256 max_eth_sold, uint256 deadline, address recipient, address exchange_addr) external returns (uint256  tokens_sold);
-}
-interface IUniswapV2Factory {
-  event PairCreated(address indexed token0, address indexed token1, address pair, uint);
-
-  function getPair(address tokenA, address tokenB) external view returns (address pair);
-  function allPairs(uint) external view returns (address pair);
-  function allPairsLength() external view returns (uint);
-
-  function feeTo() external view returns (address);
-  function feeToSetter() external view returns (address);
-
-  function createPair(address tokenA, address tokenB) external returns (address pair);
 }
 interface IUniswapV2Router01 { //Uniswap V2 on Ropsten 0xf164fC0Ec4E93095b804a4795bBe1e041497b92a
     function WETH() external pure returns (address);
@@ -81,8 +41,12 @@ interface aaveLinks {
 
 contract flashTurnaround {
 
-    address ETH=0xEeeeeEeeeEeEeeEeEeEeeEEEeeeeEeeeeeeeEEeE;
-    address aaveAddr=0x1c8756FD2B28e9426CDBDcC7E3c4d64fa9A54728; // get addresses from Aave Addresses Provider
+    // Addresses of all relevant exchanges
+    address ETH=0xEeeeeEeeeEeEeeEeEeEeeEEEeeeeEeeeeeeeEEeE; // as used by Aave
+    address aaveAddr=0x1c8756FD2B28e9426CDBDcC7E3c4d64fa9A54728; // hub to get further addresses from Aave Addresses Provider
+    address kyberSC=0x818E6FECD516Ecc3849DAf6845e3EC868087B755;
+    address uniswapV1factory=0x9c83dCE8CA20E9aAF9D3efc003b2ea62aBC08351;  // Factory address - Mainnet: 0xc0a47dFe034B400B47bDaD5FecDa2621de6c4d95  
+    address uniswapV2router=0xf164fC0Ec4E93095b804a4795bBe1e041497b92a;
 
     fallback() external payable {}
     receive() external payable {}
@@ -123,24 +87,25 @@ contract flashTurnaround {
     }
 
     function performSwaps(bytes memory params, uint fee) private {
-        address[] memory tokenPath; string[] memory exchangePath; uint injectedAmount; uint tradeAmount;
+        address[] memory tokenPath; string[] memory exchangePath; uint injectedAmount; uint tradeAmount; uint xAmount=tradeAmount;
         (tokenPath, exchangePath, injectedAmount, tradeAmount) = abi.decode(params, (address[], string[], uint, uint));
 
         address targetAddr=tokenPath[0]; // changes along the tokenPath
         for (uint i=0; i<tokenPath.length-1; i++) {
             address fromAddr=tokenPath[i]; targetAddr=tokenPath[i+1];
-            if (hashi(exchangePath[i])==hashi("kyberswap")) tradeAmount = kyberSwap(fromAddr, targetAddr, tradeAmount);
-            if (hashi(exchangePath[i])==hashi("uniswapv1")) tradeAmount = uniswapv1(fromAddr, targetAddr, tradeAmount);
-            if (hashi(exchangePath[i])==hashi("uniswapv2")) tradeAmount = uniswapv2(fromAddr, targetAddr, tradeAmount);
+            if (hashi(exchangePath[i])==hashi("kyberswap")) xAmount = kyberSwap(fromAddr, targetAddr, xAmount);
+            if (hashi(exchangePath[i])==hashi("uniswapv1")) xAmount = uniswapv1(fromAddr, targetAddr, xAmount);
+            if (hashi(exchangePath[i])==hashi("uniswapv2")) xAmount = uniswapv2(fromAddr, targetAddr, xAmount);
         }
 
-        // Return remaining Tokens to sender
-        if(targetAddr!=ETH) ERC20(targetAddr).transfer(msg.sender, tradeAmount-fee);
-        else msg.sender.transfer(tradeAmount-fee);
+        // If flashloan has been taken, reduce returned amount by flashloan amount
+        if(tradeAmount>injectedAmount) xAmount=xAmount-(tradeAmount-injectedAmount);
+        // Return remaining Tokens to sender (minus fee / margin share)
+        if(targetAddr!=ETH) ERC20(targetAddr).transfer(msg.sender, xAmount-fee);
+        else msg.sender.transfer(xAmount-fee);
     }
 
     function kyberSwap(address fromAddr, address targetAddr, uint amount) public payable returns (uint exchangedAmount) {
-        address kyberSC=0x818E6FECD516Ecc3849DAf6845e3EC868087B755;
         SimpleNetworkInterface k = SimpleNetworkInterface(kyberSC);
         if(fromAddr!=ETH) {
             ERC20(fromAddr).approve(kyberSC, amount);
@@ -153,7 +118,7 @@ contract flashTurnaround {
     }
 
     function uniswapv1(address fromAddr, address targetAddr, uint amount) public payable returns (uint exchangedAmount) {
-        UniswapFactoryInterface Factory=UniswapFactoryInterface(0x9c83dCE8CA20E9aAF9D3efc003b2ea62aBC08351); // Factory address - Mainnet: 0xc0a47dFe034B400B47bDaD5FecDa2621de6c4d95        
+        UniswapFactoryInterface Factory=UniswapFactoryInterface(uniswapV1factory);      
         if(fromAddr!=ETH) {
             address exchange=Factory.getExchange(fromAddr);
             ERC20(fromAddr).approve(exchange, amount);
@@ -174,13 +139,10 @@ contract flashTurnaround {
         return a;
     }
     function uniswapv2(address fromAddr, address targetAddr, uint amount) public payable returns (uint exchangedAmount) {
-        //address uniswapSC=0x5C69bEe701ef814a2B6a3EDD4B1652CB9cc5aA6f; // Factory address - same on Mainnet: 0x5C69bEe701ef814a2B6a3EDD4B1652CB9cc5aA6f
-        //address uniPair=IUniswapV2Factory(uniswapSC).getPair(fromAddr, targetAddr);
-        address v2router=0xf164fC0Ec4E93095b804a4795bBe1e041497b92a;
-        IUniswapV2Router01 u=IUniswapV2Router01(v2router);
+        IUniswapV2Router01 u=IUniswapV2Router01(uniswapV2router);
         address WETH=u.WETH();
         if(fromAddr!=ETH) {
-            ERC20(fromAddr).approve(v2router, amount);
+            ERC20(fromAddr).approve(uniswapV2router, amount);
             if(targetAddr!=ETH)
                 return u.swapExactTokensForTokens(amount, 0, addr(fromAddr, targetAddr), address(this), now+120)[1];
             else
@@ -193,16 +155,10 @@ contract flashTurnaround {
         return keccak256(abi.encodePacked(text));
     }
 
-    function giveMeEth() public payable {
-
+    function getEthBalance() public view returns (uint) {
+        return address(this).balance;
     }
-
-    function getBalance() public pure returns (uint256) { //view
-        return 823;
-        //address(this).balance;
-    }
-
-    function withdraw() public {
-        msg.sender.transfer(address(this).balance);
+    function getTokenBalance(address tokenAddress) public view returns (uint) {
+        return ERC20(tokenAddress).balanceOf(address(this));
     }
 }
